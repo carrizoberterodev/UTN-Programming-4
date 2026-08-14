7e413663f05e7adf2dd0570fa58d05f8de6e8904
Franco <carrizobertero.dev@gmail.com>
2026-08-14 12:26:45 -0300

    Creacion del proyecto
    Se creo el proyecto y dio estructura al principio de lo que pide el trabajo practico.

---
+++ TP1_GRUPO_5/src/ejercicio1/Persona.java
@@ -0,0 +1,32 @@
+package ejercicio1;
+
+public class Persona {
+	private String 	dni;
+	private String 	nombre;
+	private String 	apellido;
+	//faltan mas atributos
+	
+	
+	//constructores
+	Persona(){} //constructor vacio
+	
+	//getters y setters
+	public String getDni() {
+		return dni;
+	}
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getApellido() {
+		return apellido;
+	}
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}	
+}
---
+++ TP1_GRUPO_5/src/ejercicio1/Principal.java
@@ -0,0 +1,14 @@
+package ejercicio1;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Persona persona = new Persona();
+		
+		persona.setNombre("franco");
+		
+		System.out.println(persona.getNombre());
+	}
+
+}
