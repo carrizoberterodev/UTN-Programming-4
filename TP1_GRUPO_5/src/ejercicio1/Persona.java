e02710f243b6b008b3c87f12600634161cef48a6
Marina <marina.2001.rodriguez@gmail.com>
2026-08-15 01:31:29 -0300

    Agrego aporte de enzo + termino los atributos de la Persona

--- TP1_GRUPO_5/src/ejercicio1/Persona.java
+++ TP1_GRUPO_5/src/ejercicio1/Persona.java
@@ -1,14 +1,25 @@
 package ejercicio1;
 
+import java.time.LocalDate;
+
 public class Persona {
 	private String 	dni;
 	private String 	nombre;
 	private String 	apellido;
-	//faltan mas atributos
+	private int edad;
+	private LocalDate fechaNacimiento;
+	private String genero;
+	private String direccion;
+	private String telefono;
+	private String email;
+
+	//atributos listos
 	
 	
 	//constructores
 	Persona(){} //constructor vacio
+	
+	// faltaria realizar otro constructor
 	
 	//getters y setters
 	public String getDni() {
@@ -28,5 +39,60 @@ public class Persona {
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
-	}	
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	// metodo toString (modificable)
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad
+				+ ", fechaNacimiento=" + fechaNacimiento + ", genero=" + genero + ", direccion=" + direccion
+				+ ", telefono=" + telefono + ", email=" + email + "]";
+	}
+	
+	
+	
+	
 }
