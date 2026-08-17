f778ac7c14cc1b917f67fde362e18044085a92f5
Franco <carrizobertero.dev@gmail.com>
2026-08-17 17:37:35 -0300

    Desarrollo de TP

--- TP1_GRUPO_5/src/ejercicio1/Persona.java
+++ TP1_GRUPO_5/src/ejercicio1/Persona.java
@@ -1,14 +1,50 @@
 package ejercicio1;
 
+import java.time.LocalDate;
+
 public class Persona {
 	private String 	dni;
 	private String 	nombre;
 	private String 	apellido;
-	//faltan mas atributos
+	private int edad;
+	private LocalDate fechaNacimiento;
+	private String genero;
+	private String direccion;
+	private String telefono;
+	private String email;
+	
+	//----------CONSTRUCTORES----------
+	
+	//constructor vacio
+	public Persona(){
+		setNombre("sin nombre");
+		setEdad(99);
+	} 
 	
 	
-	//constructores
-	Persona(){} //constructor vacio
+	// constructor con parametros
+	public Persona(String Dni, String Nombre, String Apellido,
+			LocalDate FechaNacimiento, String Genero, String Direccion, String Telefono, String Email ){
+		
+		setDni(Dni);
+		setNombre(Nombre);
+		setApellido(Apellido);
+		setFechaNacimiento(FechaNacimiento);
+		setGenero(Genero);
+		setDireccion(Direccion);
+		setTelefono(Telefono);
+		setEmail(Email);
+	    LocalDate hoy = LocalDate.now();
+	    int edadCalculada = hoy.getYear() - fechaNacimiento.getYear();
+	    if (fechaNacimiento.getMonthValue() > hoy.getMonthValue() ||
+	        (fechaNacimiento.getMonthValue() == hoy.getMonthValue() && fechaNacimiento.getDayOfMonth() > hoy.getDayOfMonth())) 
+	    {
+	        edadCalculada--;
+	    }
+	    setEdad(edadCalculada);
+	}
+	//---------------------------------
+	
 	
 	//getters y setters
 	public String getDni() {
@@ -28,5 +64,60 @@ public class Persona {
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
-	}	
+	}
+	public int getEdad() {
+		return edad;
+	}
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	public LocalDate getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+	public void setFechaNacimiento(LocalDate fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	public String getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	
+	// metodo toString (modificable)
+	@Override
+	public String toString() {
+		return "Persona [dni=" + dni + ", nombre=" + nombre + ", apellido=" + apellido + ", edad=" + edad
+				+ ", fechaNacimiento=" + fechaNacimiento + ", genero=" + genero + ", direccion=" + direccion
+				+ ", telefono=" + telefono + ", email=" + email + "]";
+	}
+	
+	
+	
+	
 }
--- TP1_GRUPO_5/src/ejercicio1/Principal.java
+++ TP1_GRUPO_5/src/ejercicio1/Principal.java
@@ -4,11 +4,7 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Persona persona = new Persona();
-		
-		persona.setNombre("franco");
-		
-		System.out.println(persona.getNombre());
+	
 	}
 
 }
